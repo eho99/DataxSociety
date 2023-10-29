546e0e5630a2fc944ea849455cc70a366b4ab827
Eric Ho <ericho@berkeley.edu>
2023-10-29 02:27:50 -0700

    feature dev: finished data and model schema with relational mapping to project

--- DataxSociety/models/mnistdata.py
+++ DataxSociety/models/mnistdata.py
@@ -8,5 +8,5 @@ class MNISTData(rx.Model, table=True):
     """MNIST Data Model"""
     id: Optional[int] = Field(default=None, primary_key=True)
     creator_id: int = Field(default=None, foreign_key="userprofile.id") # Many to One
-    pixel_vals: List[List[float]] = Field(default=None) # 2D array of brightness values for mnist
+    pixel_vals: List[float] = Field(default=None) # 1D array of brightness values for mnist
     label: int = Field(default=None)
--- DataxSociety/models/project.py
+++ DataxSociety/models/project.py
@@ -14,7 +14,7 @@ class Project(rx.Model, table=True):
     num_datapoints: Optional[int] = Field(default=None) # if None, then unlimited data points are allowed
     num_indep_vars: int = Field(default=0)
     num_dep_vars: int = Field(default=2)
-    best_model_id: Optional[int] = Field(default=None, foreign_key="mnistnetwork.id") # will need to update as it gose
+    best_model_id: Optional[int] = Field(default=None, foreign_key="mnistnetwork.id") # will need to update as it goes
     
     creator_id: int = Field(default=None, foreign_key="userprofile.id") # Many to One 
     project_contributors: Optional[List[UserProfile]] = Relationship(back_populates="user_contributions", link_model=UserProfileProjectLink) # Many to Many relationship
