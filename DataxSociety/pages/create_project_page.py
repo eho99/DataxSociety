e6ae674ebeaf874ecbc99215d0df947947fa4a7c
sushanthsk2004 <sushanthsk2004@berkeley.edu>
2023-10-29 06:38:01 -0700

    feature dev: constructed working project creation state backend with relational mapping to user

--- DataxSociety/pages/create_project_page.py
+++ DataxSociety/pages/create_project_page.py
@@ -1,8 +1,23 @@
 import reflex as rx
 
+from ..models.user import User
+
 from ..state.create_project_state import CreateProjectState
 from ..state.login_state import require_login
 from ..components.navbar import navbar
+
+
+def get_users():
+    with rx.session() as session:
+        users = session.exec(
+            session.query(User).with_entities(User.username)
+        ).one_or_none()
+    return users
+
+users = get_users()
+users = list(users)
+if not users:
+    users = []
 
 def create_project_page() -> rx.Component:
 
@@ -44,6 +59,17 @@ def create_project_page() -> rx.Component:
             rx.box(
                 rx.input(id="num_data", type="number", class_name="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"),
                 class_name="mt-2"
+            ),
+        ),
+        rx.form_control(
+            rx.form_label("User", html_for="username", class_name="block text-sm font-medium leading-6 text-gray-900"),
+            rx.box(
+                rx.select(
+                    users,
+                    placeholder="Select your username",
+                    id="username"
+                ),
+                class_name="mt-2",
             ),
         ),
         rx.button("Create Project", type_="submit", class_name="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"),
--- DataxSociety/state/create_project_state.py
+++ DataxSociety/state/create_project_state.py
@@ -2,6 +2,9 @@ import reflex as rx
 
 from .base_state import State
 from ..models.user import User
+from ..models.project import *
+from ..models.auth_session import AuthSession
+import datetime
 
 
 class CreateProjectState(State):
@@ -16,7 +19,33 @@ class CreateProjectState(State):
         num_indep_vars = form_data["num_indep_vars"]
         num_dep_vars = form_data["num_dep_vars"]
         num_data = form_data["num_data"]
-        
+
+        creator = form_data["username"]
+
+        with rx.session() as session:
+            result = session.exec(
+                session.query(User).where(User.username == creator)
+            ).first()
+            print(result)
+            creator_id = result[0].id
+        contributors = []
+
+
+        new_proj = Project()
+        new_proj.project_name = project_name
+        new_proj.description = description
+        new_proj.num_datapoints = num_data
+        new_proj.num_indep_vars = num_indep_vars
+        new_proj.num_dep_vars = num_dep_vars
+        new_proj.creator_id = creator_id
+        new_proj.project_contributors = contributors
+
+        with rx.session() as session:
+            session.add(Project(project_name=new_proj.project_name, description=new_proj.description,
+                                num_datapoints=new_proj.num_datapoints, num_indep_vars=new_proj.num_indep_vars, 
+                                num_dep_vars=new_proj.num_dep_vars, best_model_id=new_proj.best_model_id, creator_id=new_proj.creator_id,
+                                project_contributors=new_proj.project_contributors))
+            session.commit()
         
         return CreateProjectState.redir()  
 
