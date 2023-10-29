8116ef24c844de376a7f1c3b8b9395f7e0591d78
Abein Abraham <63220193+abeinn@users.noreply.github.com>
2023-10-29 00:29:09 -0700

    Delete .web directory

--- .web/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}"],
-	theme: {},
-	plugins: [
-		require("@tailwindcss/forms"),
-		require("@tailwindcss/typography"),
-	],
-};
